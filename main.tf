795d16e1b9b224b8f921d68db254ecc365a57c46
Lakshmi Suma Divvela <lakshmi.suma@ibm.com>
2023-04-05 19:13:59 +0530

    a

--- main.tf
+++ main.tf
@@ -30,6 +30,7 @@ resource "ibm_is_subnet" "subnet1" {
   name = "subnet-1"  
   vpc =ibm_is_vpc.example.id  
   zone="eu-de-1"
+  ipv4_cidr_block = "10.240.0.0/24"
   # zone = "us-south-1" 
   # total_ipv4_address_count = 256
   resource_group=var.resource_group
