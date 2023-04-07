2da0e274b7a1169467f64490e4930491e95c8d18
Lakshmi Suma Divvela <lakshmi.suma@ibm.com>
2023-04-07 10:22:57 +0530

    a

--- main.tf
+++ main.tf
@@ -9,34 +9,34 @@ resource "ibm_is_vpc" "example" {
 
 
 
-resource "ibm_container_vpc_cluster" "cluster" {
-  name                   = "test-cluster"
-  vpc_id                 = ibm_is_vpc.example.id
-  flavor                 = "bx2.16x64"
-  worker_count           = 1
-  kube_version           = "1.26.3"
-  update_all_workers     = true
-  wait_for_worker_update = true
-  resource_group_id=var.resource_group_id
+# resource "ibm_container_vpc_cluster" "cluster" {
+#   name                   = "test-cluster"
+#   vpc_id                 = ibm_is_vpc.example.id
+#   flavor                 = "bx2.16x64"
+#   worker_count           = 1
+#   kube_version           = "1.26.3"
+#   update_all_workers     = true
+#   wait_for_worker_update = true
+#   resource_group_id=var.resource_group_id
 
-  zones {
+#   zones {
     
-      subnet_id = ibm_is_subnet.subnet1.id
-      name      =  "us-south-1"
-  }
-}
+#       subnet_id = ibm_is_subnet.subnet1.id
+#       name      =  "us-south-1"
+#   }
+# }
 
-resource "ibm_is_subnet" "subnet1" {  
-  name = "subnet-1"  
-  vpc =ibm_is_vpc.example.id  
-  # zone="eu-de-1"
-  # ipv4_cidr_block = "10.0.1.0/24"
-  zone = "us-south-1" 
-  # ipv4_cidr_block = "10.240.0.0/24"
-   total_ipv4_address_count = 256
-  resource_group=var.resource_group
-  # cidr_block          = "10.0.1.0/24"
-  # ipv4_cidr_block     = cidrsubnet("10.0.1.0/24", 8, 2)
- }
+# resource "ibm_is_subnet" "subnet1" {  
+#   name = "subnet-1"  
+#   vpc =ibm_is_vpc.example.id  
+#   # zone="eu-de-1"
+#   # ipv4_cidr_block = "10.0.1.0/24"
+#   zone = "us-south-1" 
+#   # ipv4_cidr_block = "10.240.0.0/24"
+#    total_ipv4_address_count = 256
+#   resource_group=var.resource_group
+#   # cidr_block          = "10.0.1.0/24"
+#   # ipv4_cidr_block     = cidrsubnet("10.0.1.0/24", 8, 2)
+#  }
 
 
