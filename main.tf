f7b0b19e349f612fe1c804d7fcb569071f68ffae
Lakshmi Suma Divvela <lakshmi.suma@ibm.com>
2023-05-08 12:52:35 +0530

    first

--- main.tf
+++ main.tf
@@ -74,7 +74,8 @@ output "ip_address1" {
   
 }
 output "ip_address2" {
-  value=concat(var.ips,lookup(data.ibm_container_vpc_cluster_worker.worker2.network_interfaces[0],"ip_address",""))
+  value=concat(var.ips,(lookup(data.ibm_container_vpc_cluster_worker.worker2.network_interfaces[0],"ip_address","")))
+  
   depends_on = [ data.ibm_container_vpc_cluster_worker.worker2 ]
   
 }
